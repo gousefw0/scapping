84f0cefe0cba1ae72d91f130b4e5aafa3c412124
youssef walid mahamed adel <73849250+gousefw0@users.noreply.github.com>
2023-02-02 16:15:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ import requests
 app = Flask(__name__)
 from markupsafe import escape
 ###########################
-@app.route("/")
-def home():
-        url = 'https://english.mubasher.info/markets/EGX/stocks/' + 'abuk' + '/'
+@app.route("/stock/<stock>")
+def stock(stock):
+        url = 'https://english.mubasher.info/markets/EGX/stocks/' + stock + '/'
         result = requests.get(url)
         src = result.content
         soup = BeautifulSoup(src,'lxml')
         my_stock_data = {}
         my_stock_data['stock_price'] =soup.find('div',{'class' : 'market-summary__last-price down-icon-only'}).text
         return jsonify(my_stock_data)
+@app.route("/")
+def home():
+    data = {'page':'home page','message':'ok'}
+    return jsonify(data)
 ###########################
 app.run(debug=False,host='0.0.0.0')
