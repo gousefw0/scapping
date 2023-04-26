f74479b86c2017c72cb6b2323feb1fb73d69a1c2
youssef walid mahamed adel <73849250+gousefw0@users.noreply.github.com>
2023-04-26 23:59:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,jsonify,request
 import json
+from json import loads, dumps
+from tvDatafeed import TvDatafeed,Interval
 from bs4 import BeautifulSoup
 from flask_cors import CORS
 from time import sleep
@@ -10,50 +12,14 @@ CORS(app)
 from markupsafe import escape
 @app.route("/stock/<stock>")
 def stock(stock):
-        url = 'https://english.mubasher.info/markets/EGX/stocks/' + stock + '/'
-        result = requests.get(url)
-        src = result.content
-        soup = BeautifulSoup(src,'lxml')
-        my_stock_data = {}
-        price=soup.find('div',{'class' : 'market-summary__last-price'}).text
-        my_stock_data["price"]=price
-        if price=="0.00" :
-                price=soup.find_all('div',{'class' : 'market-summary__block-row'})
-                price=price[1].find('span',{'class':"market-summary__block-number"}).text
-                my_stock_data["price"]=price
-        
-        url='https://www.mubasher.info/markets/EGX/stocks/'+stock+'/profile'
-        result = requests.get(url)
-        src = result.content
-        soup = BeautifulSoup(src,'lxml')
-        data =soup.find_all('span',{'class' : 'company-profile__general-information__text2'})
-        logo=soup.find('div',{'class' : 'company-profile__general-information__logo'})
-        logo=logo.find('img')['src']
-        about=data[1].text
-        txt=data[0].text
-        i=txt.find('(')
-        name=txt[:i-1]
-        ramz=txt[i+1:-1]
-        my_stock_data['name']=name
-        my_stock_data['ramz']=ramz
-        my_stock_data['about']=about
-        my_stock_data['logo']=logo
-        url='https://www.mubasher.info/markets/EGX/stocks/'+stock+'/news'
-        result = requests.get(url)
-        src = result.content
-        soup = BeautifulSoup(src,'lxml')
-        data =soup.find_all('div',{'class' : 'mi-article-media-block__content'})
-        news={}
-        l=[]
-        for x in data :
-                 l.append({'title':x.find('a',{'class':'mi-article-media-block__title'}).text
-                          ,'des':x.find('div',{'class':'mi-hide-for-small mi-article-media-block__text'}).text
-                          ,'date':x.find('span',{'class':'mi-article-media-block__date'}).text
-                          ,'link':'https://www.mubasher.info'+x.find('a',{'class':'mi-article-media-block__title'})['href']
-                         })
-            
-        my_stock_data['news']=l
-        return jsonify(my_stock_data)
+        username = 'yousef014'
+        password = 'yosef@123'
+        tv = TvDatafeed(username=username,password=password)
+        df=tv.get_hist(stock,'EGX',)
+        result = df.to_json(orient="split")
+        parsed = loads(result)
+        dumps(parsed, indent=4)  
+        return jsonify(parsed)
 @app.route("/")
 def home():
     data = {'page':'home page','message':'ok'}
